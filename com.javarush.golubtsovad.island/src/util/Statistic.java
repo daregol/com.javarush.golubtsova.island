6a8699e10f9a7c970d1c3b3a0ad2417a9a215469
Darya Golubtsova <daregol@mail.ru>
2025-01-30 00:45:54 +0700

    статистика

---
+++ com.javarush.golubtsovad.island/src/util/Statistic.java
@@ -0,0 +1,66 @@
+package util;
+
+import entity.Island;
+import entity.Location;
+import entity.creature.animal.Animal;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Statistic {
+
+    private Island island;
+
+    public Statistic(Island island) {
+        this.island = island;
+    }
+
+    public void print() {
+        printStatistics();
+        printMap();
+    }
+
+    private void printStatistics() {
+        int totalAnimals = 0;
+        int totalPlants = 0;
+
+        for (int x = 0; x < Settings.columnsCount; x++) {
+            for (int y = 0; y < Settings.rowsCount; y++) {
+                Location loc = island.getLocation(x, y);
+                List<Animal> animals = new ArrayList<>();
+                for (CreatureType creatureType : CreatureType.values()) {
+                    if(creatureType == CreatureType.PLANT) { continue; }
+                    animals.addAll(loc.getAnimalsMap().get(creatureType));
+                }
+                totalAnimals += animals.size();
+                totalPlants += loc.getPlant().getQuantity();
+
+            }
+        }
+
+        System.out.println("[СТАТИСТИКА] Животных: " + totalAnimals + ", Растений: " + totalPlants);
+    }
+
+    private void printMap() {
+        System.out.println("=== Карта острова ===");
+        for (int x = 0; x < Settings.columnsCount; x++) {
+            for (int y = 0; y < Settings.rowsCount; y++) {
+                Location loc = island.getLocation(x, y);
+                List<Animal> animals = new ArrayList<>();
+                for (CreatureType creatureType : CreatureType.values()) {
+                    if(creatureType == CreatureType.PLANT) { continue; }
+                    animals.addAll(loc.getAnimalsMap().get(creatureType));
+                }
+
+                if (!animals.isEmpty() && !(loc.getPlant().getQuantity() == 0)) {
+                    System.out.print(animals.get(0).getImage());
+
+                } else if (!(loc.getPlant().getQuantity() == 0)) {
+                    System.out.print("🌱");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println("======================");
+    }
+}
